cb3dbda457e22e42317f8f6508ada3f20e07a507
rabinul <rabinul2017@gmail.com>
2024-04-11 03:11:18 +0600

    -- home index

---
+++ home_components/articles.php
@@ -0,0 +1,32 @@
+<article>
+    <h3>Quality Automotive Repair Services</h3>
+    <p>When you’re in the market for a quality auto repair shop you can count on, top-notch services and affordable prices mean everything.</p>
+    <p>You also want to know that you’re taking your car to an experienced auto mechanic with years of experience.</p>
+    <p>Thankfully, the drivers of Fort Worth, Texas, and the surrounding areas can trust the seasoned specialists at JD Automotive Service
+        for all of their auto service needs.</p>
+    <p>Whether your car needs new brake pads or you hear strange noises underneath the hood, our team of ASE-certified mechanics is here to help.</p>
+    <p>We are open from 8 am to 6 pm, Monday through Friday, and from 8 am to 3 pm on Saturday. For car care services you can trust, call us today at 817-451-8066.</p>
+</article>
+
+<article>
+    <h3 class="card-title">Does Your Car Need Auto Repair Services?</h3>
+    <p>Even if you’ve been driving for decades, it can be tough to tell if your vehicle needs auto repair services.</p>
+    <p>Luckily, there are a few telltale signs that your car needs to be inspected by an ASE-certified mechanic.</p>
+    <p>Is your car stalling or struggling to start? Is your check engine light (CEL) illuminated on your dashboard?</p>
+    <p>Or is your CEL flickering? Call the automotive repair experts at JD Automotive Service in Fort Worth, Texas, today.</p>
+    <p>For over 16 years, we have been helping our customers with their automotive service and maintenance needs including brake repair, and more.</p>
+    <p>A few other red flags that your car needs to be looked at by a qualified mechanic include excessive exhaust smoke, smoke coming from beneath the hood,
+        reduced stopping power, any noticeable leaks, poor gas mileage, a shaking steering wheel, slipping gears, or loud grinding or squealing sounds coming from the brakes.</p>
+    <p>If you notice any of these issues, call JD Automotive Service now. We will quickly get to work diagnosing and repairing your vehicle.</p>
+</article>
+
+<article>
+    <h3>WHEN YOU NEED COMPLETE CAR REPAIR FROM THE EXPERTS, TURN TO FIRESTONE</h3>
+
+    <p>Maintaining your vehicle's health is always in your best interest. Cars that are well taken care of can outperform and outlast those that are not.
+    When something feels “off” with your vehicle, don't hesitate to bring it to Firestone Complete Auto Care. Our expert auto technicians will assess your vehicle and provide full service auto repairs.</p>
+    <p>Any time you visit Firestone Complete Auto Care for repair services, we offer a complimentary courtesy check at no cost to you. Our expert technicians inspect your vehicle's major operating systems –your tires, fluid levels, battery, lights, wipers, belts, hoses, and more.</p>
+    <p>Our auto advisors will explain any suggested maintenance or repairs before starting work on your vehicle to give you the information you need to make the best decision for your vehicle.</p>
+    <p>Firestone Complete Auto Care offers honest, reliable car care, and our auto technicians look out for your vehicle and your budget, which means we'll never surprise you with any out-of-the-blue car repairs.</p>
+
+</article>
---
+++ home_components/menu.php
@@ -0,0 +1,133 @@
+
+<nav class="container-fluid m-0 p-0">
+    <ul class="nav flex-row justify-content-end" id="baseToolBar">
+        <li><h5><a href="#" id="registerToolbarLink" class="nav-link" data-bs-toggle="modal" data-bs-target="#registerModal"><i class="fa-solid fa-user-plus"></i> Create an account</a></h5></li>
+        <li><h5><a href="#" id="loginToolbarLink" class="nav-link" data-bs-toggle="modal" data-bs-target="#loginModal"><i class="fa-solid fa-right-to-bracket"></i> Login</a></h5></li>
+    </ul>
+</nav>
+
+<div class="m-0 p-5 bg-body-tertiary text-body" id="jumbotron">
+    <h1 class="pt-5">Auto Repair Services</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..</p>
+</div>
+
+<nav class="navbar navbar-expand-lg bg-light sticky-top shadow-sm">
+    <div class="container-fluid">
+        <i class="fa-solid fa-blog d-sm-block d-lg-none d-xl-block"></i>
+        <a class="navbar-brand px-2 d-sm-block d-lg-none d-xl-block" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto" style="min-width: 790px;">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="../"><i class="fa-solid fa-house"></i> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-circle-info"></i> About</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-wrench"></i> Services
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Auto Repair Services</a></li>
+                        <li><a class="dropdown-item" href="#">Brake Repair</a></li>
+                        <li><a class="dropdown-item" href="#">Auto A/C Repair</a></li>
+                        <li><a class="dropdown-item" href="#">Suspension Repair</a></li>
+                        <li><a class="dropdown-item" href="#">Check Engine Light Diagnostic</a></li>
+                        <li><a class="dropdown-item" href="#">Oil Change</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Estimation</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-star"></i> Special</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-gears"></i> Parts Store</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-warehouse"></i> Find Nearby Garage</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-phone"></i> Contacts</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary ml-2" type="submit">Search</button>
+            </form>
+
+            <!-- modal dialog for login -->
+            <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content shadow-lg">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="container-fluid">
+                                <form class="form-signin">
+                                    <div class="form-floating">
+                                        <input tabindex="1" type="text" class="form-control" id="usernameField" placeholder="Username" autofocus>
+                                        <label for="usernameField">Username</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input tabindex="2" type="password" class="form-control" id="passwordField" placeholder="Password">
+                                        <label for="passwordField">Password</label>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col">
+                                            <div class="form-check form-switch">
+                                                <input tabindex="3" class="form-check-input" type="checkbox" role="switch" id="stayLoggedSwitch">
+                                                <label class="form-check-label" for="stayLoggedSwitch">Stay logged in</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3 m-1">
+                                        <input tabindex="4" type="button" id="loginButton" class="mb-1 btn btn-lg bg-primary" value="Login">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <h5>Not a member yet? <a class="link-primary" href="#">Sign up</a></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal dialog for registering -->
+            <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModal" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content shadow-lg">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Registration</h5>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Select which type of account do you want to create</p>
+                            <ul>
+                                <li><strong>Customer Account: </strong> for typical users.</li>
+                                <li><strong>Business Account: </strong> specialized car mechanic, for garage owners, spare parts dealer.</li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Consumer Account</button>
+                            <button type="button" class="btn btn-secondary">Business Account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</nav>
+
